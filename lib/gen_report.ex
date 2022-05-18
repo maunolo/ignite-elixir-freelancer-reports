d1fb7f96d122f1ebb184e6781dc8d42f8f4c8846
Mauricio Lopes <mauricionlopes.dev@gmail.com>
2022-05-17 23:43:25 -0300

    Finishing module 2 challenge 1

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,84 @@
 defmodule GenReport do
+  alias GenReport.Parser
 
-  def build do
-    # TO DO
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), &sum_values(&1, &2))
+  end
+
+  def build, do: {:error, "Insira o nome de um arquivo"}
+
+  # def build_from_many(filenames) when not is_list(filenames) do
+  #   {:error, "Please provide a list of strings"}
+  # end
+
+  # def build_from_many(filenames) do
+  #   result =
+  #     filenames
+  #     |> Task.async_stream(&build/1)
+  #     |> Enum.reduce(report_acc(), fn {:ok, result}, report -> sum_reports(report, result) end)
+
+  #   {:ok, result}
+  # end
+
+  defp sum_values([name, hours, _day, month, year], %{
+         "all_hours" => all_hours,
+         "hours_per_month" => hours_per_month,
+         "hours_per_year" => hours_per_year
+       }) do
+    all_hours = Map.put(all_hours, name, sum(all_hours[name], hours))
+
+    hours_per_month =
+      Map.put(hours_per_month, name, sum_hours_by_month(hours_per_month[name], hours, month))
+
+    hours_per_year =
+      Map.put(hours_per_year, name, sum_hours_by_year(hours_per_year[name], hours, year))
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  # defp sum_reports(
+  #        %{"foods" => foods1, "users" => users1},
+  #        %{"foods" => foods2, "users" => users2}
+  #      ) do
+  #   foods = merge_maps(foods1, foods2)
+  #   users = merge_maps(users1, users2)
+
+  #   build_report(foods, users)
+  # end
+
+  defp merge_maps(map1, map2),
+    do: Map.merge(map1, map2, fn _key, value1, value2 -> value1 + value2 end)
+
+  defp sum(nil, new), do: new
+  defp sum(previous, new), do: previous + new
+
+  defp sum_hours_by_month(nil, hours, month), do: %{month => hours}
+
+  defp sum_hours_by_month(previous, hours, month) do
+    merge_maps(previous, %{month => hours})
+  end
+
+  defp sum_hours_by_year(nil, hours, year), do: %{year => hours}
+
+  defp sum_hours_by_year(previous, hours, year) do
+    merge_maps(previous, %{year => hours})
+  end
+
+  def report_acc do
+    all_hours = %{}
+    hours_per_month = %{}
+    hours_per_year = %{}
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      "all_hours" => all_hours,
+      "hours_per_month" => hours_per_month,
+      "hours_per_year" => hours_per_year
+    }
   end
 end
---
+++ lib/parser.ex
@@ -0,0 +1,33 @@
+defmodule GenReport.Parser do
+  @months [
+    "janeiro",
+    "fevereiro",
+    "março",
+    "abril",
+    "maio",
+    "junho",
+    "julho",
+    "agosto",
+    "setembro",
+    "outubro",
+    "novembro",
+    "dezembro"
+  ]
+
+  def parse_file(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Stream.map(&parse_line/1)
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.downcase()
+    |> String.split(",")
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &Enum.at(@months, String.to_integer(&1) - 1))
+    |> List.update_at(4, &String.to_integer/1)
+  end
+end
